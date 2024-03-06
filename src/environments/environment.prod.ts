a1e81ba346ce8647d6852674bd07685e49ab3b37
Chris Albrecht <vegashat@gmail.com>
2024-03-06 15:23:00 -0600

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export const environment = {
       { title: "CNN", siteUrl: "https://www.cnn.com", feedUrl: "https://cors.vegashat.com/http://rss.cnn.com/rss/cnn_latest.rss" },
       { title: "ESPN", siteUrl: "https://www.espn.com", feedUrl: "https://cors.vegashat.com/http://www.espn.com/espn/rss/news" },
       { title: "Engadget", siteUrl: "https://www.engadget.com", feedUrl: "https://cors.vegashat.com/https://www.engadget.com/rss.xml" },
-      { title: "Lifehacker", siteUrl: "http://lifehacker.com", feedUrl: "https://cors.vegashat.com/http://lifehacker.com/rss" },
-      { title: "Slashdot", siteUrl: "https://www.slashdot.org", feedUrl: "https://cors.vegashat.com/http://rss.slashdot.org/Slashdot/slashdot" }
+      { title: "Lifehacker", siteUrl: "http://lifehacker.com", feedUrl: "http://lifehacker.com/rss" },
+      { title: "Slashdot", siteUrl: "https://www.slashdot.org", feedUrl: "http://rss.slashdot.org/Slashdot/slashdot" }
     ]
 };
